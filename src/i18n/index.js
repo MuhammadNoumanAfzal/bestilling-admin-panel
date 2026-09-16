0f5fc96a710cb8cdf8a68efbdefe468a744ca121
MuhammadNoumanAfzal <mnoumanafzal4@gmail.com>
2026-09-16 15:39:28 +0500

    feat: update language settings to default to Norwegian and improve i18n configuration

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -17,11 +17,12 @@ import commission from "./commission";
 import payouts from "./payouts";
 
 const LANGUAGE_STORAGE_KEY = "admin-panel-language";
+const DEFAULT_LANGUAGE = "nb";
 const supportedLanguages = ["nb", "en"];
 
 function resolveInitialLanguage() {
   if (typeof window === "undefined") {
-    return "nb";
+    return DEFAULT_LANGUAGE;
   }
 
   const savedLanguage = window.localStorage.getItem(LANGUAGE_STORAGE_KEY);
@@ -29,7 +30,7 @@ function resolveInitialLanguage() {
     return savedLanguage;
   }
 
-  return "nb";
+  return DEFAULT_LANGUAGE;
 }
 
 const initialLanguage = resolveInitialLanguage();
@@ -37,7 +38,7 @@ const initialLanguage = resolveInitialLanguage();
 i18n.use(initReactI18next).init({
   resources: { nb: { translation: nb, adminSettings: settings, adminPayouts: payouts, adminCommission: commission, adminDelivery: delivery, adminNotifications: notifications, adminHomeCuration: homeCuration, adminVendorSettings: vendorSettings, adminSupport: support, adminReports: reports, adminCustomers: customers, adminOrders: orders, adminVendors: vendors, adminShell: shell }, en: { translation: en, adminSettings: Object.fromEntries(Object.keys(settings).map(key => [key, key])), adminPayouts: Object.fromEntries(Object.keys(payouts).map(key => [key, key])), adminCommission: Object.fromEntries(Object.keys(commission).map(key => [key, key])), adminDelivery: Object.fromEntries(Object.keys(delivery).map(key => [key, key])), adminNotifications: Object.fromEntries(Object.keys(notifications).map(key => [key, key])), adminHomeCuration: Object.fromEntries(Object.keys(homeCuration).map(key => [key, key])), adminVendorSettings: Object.fromEntries(Object.keys(vendorSettings).map(key => [key, key])), adminSupport: Object.fromEntries(Object.keys(support).map(key => [key, key])), adminReports: Object.fromEntries(Object.keys(reports).map(key => [key, key])), adminCustomers: Object.fromEntries(Object.keys(customers).map(key => [key, key])), adminOrders: Object.fromEntries(Object.keys(orders).map(key => [key, key])), adminVendors: Object.fromEntries(Object.keys(vendors).map(key => [key, key])), adminShell: Object.fromEntries(Object.keys(shell).map(key => [key, key])) } },
   lng: initialLanguage,
-  fallbackLng: "nb",
+  fallbackLng: DEFAULT_LANGUAGE,
   supportedLngs: supportedLanguages,
   interpolation: { escapeValue: false },
   react: { useSuspense: false },
